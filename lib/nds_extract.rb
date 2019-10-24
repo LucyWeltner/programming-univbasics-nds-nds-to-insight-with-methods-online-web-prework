7c691d1908626725aa9b6798c2ecb6eafe04b1c5
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-24 18:26:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,19 +1,24 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
-def gross_for_director(nds, directornum)
+def gross_for_director(nds, director)
   moviecounter = 0 
-  total_gross = 0
+  gross_of_director = 0
   number_of_movies = nds[directornum][:movies].length
   while moviecounter < number_of_movies do
-    total_gross += 
-  
+    gross_of_movie = nds[director][:movies][moviecounter][:worldwide_gross]
+    gross_of_director += gross_of_movie
+    moviecounter += 1
+  end 
+  return gross_of_director 
+end
 
 def directors_totals(nds)
   i = 0 
   j = 0
   directors_hash = {}
   while i < nds.length do
+    gross_for_director(nds, i)
     while j < nds[i][:movies].length do
       p nds[i][:movies][j]
       money = nds[i][:movies][j][:worldwide_gross]
